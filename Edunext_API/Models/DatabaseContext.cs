fef60d51bddd485fb7cb9f2d027fca47e02ab2ae
Vu Huu Tai <lkvu92@gmail.com>
2024-01-06 09:10:12 +0700

    model OD

--- Edunext_API/Models/DatabaseContext.cs
+++ Edunext_API/Models/DatabaseContext.cs
@@ -5,5 +5,17 @@ namespace Edunext_API.Models
     public class DatabaseContext : DbContext
     {
         public DatabaseContext(DbContextOptions options) : base(options) { }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<OrderDetail>()
+                .HasOne<Order>(od => od.Order)
+                .WithMany(o => o.OrderDetails)
+                .HasForeignKey(od => od.OrderID);
+            modelBuilder.Entity<OrderDetail>()
+                .HasOne<Product>(od => od.Product)
+                .WithMany(p => p.OrderDetails)
+                .HasForeignKey(od => od.ProductID);
+        }
     }
 }
--- Edunext_API/Models/OrderDetail.cs
+++ Edunext_API/Models/OrderDetail.cs
@@ -1,6 +1,41 @@
-﻿namespace Edunext_API.Models
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Edunext_API.Models
 {
     public class OrderDetail
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int OrderDetailID { get; set; }
+        [Required]
+        public int OrderID { get; set; }
+        [Required]
+        public int ProductID { get; set; }
+        [Required]
+        [Range(1, 1000,ErrorMessage = "Quantity must be between 1 and 1000")]
+        public int Quantity { get; set; }
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        [Range(0.1, 2000, ErrorMessage = "Price must be between 0.1 and 2000")]
+        public decimal UnitPrice { get; set; }
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        [Range(0.1, 100, ErrorMessage = "Discount must be between 0.1 and 100")]
+        public decimal Discount { get; set; }
+        public decimal LineTotal
+        {
+            get
+            {
+                return Quantity * UnitPrice * (1 - Discount / 100);
+            }
+        }
+        public DateTime CreatedDate { get; set; }
+
+        [JsonIgnore]
+        public Order? Order { get; set; }
+        [JsonIgnore]
+        public Product? Product { get; set; }
     }
 }
