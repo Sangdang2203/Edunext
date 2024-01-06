896b29afe27f883e2175bfc6aee844dbcc14e90e
Sangdang2203 <sangdang2203@gmail.com>
2024-01-06 18:22:12 +0700

    1st

--- Edunext_API/Models/DatabaseContext.cs
+++ Edunext_API/Models/DatabaseContext.cs
@@ -5,5 +5,6 @@ namespace Edunext_API.Models
     public class DatabaseContext : DbContext
     {
         public DatabaseContext(DbContextOptions options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
     }
 }
